8ba18304f524e8db2acb4333045ee9f1f5ad8dd5
MiklisSphinx <kimantonares69@gmail.com>
2026-04-02 20:07:21 +0800

    feat: final pwa and layout fix

--- ojtly-code/src/app/layout.tsx
+++ ojtly-code/src/app/layout.tsx
@@ -6,7 +6,7 @@ const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'OJTly',
-  description: 'OJT Management System',
+  description: 'OJTly Management App',
   manifest: '/manifest.json', 
   icons: {
     icon: '/icon.png', 
